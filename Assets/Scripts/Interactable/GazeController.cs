05b9f8a4ab25990529efe736dae0091201ec706e
Antoine Brouillette <antoineqc@hotmail.com>
2016-11-05 14:11:18 -0400

    Working on Gaze Selection

---
+++ Assets/Scripts/Interactable/GazeController.cs
@@ -0,0 +1,53 @@
+﻿#define DEBUG_RAYCAST
+
+using System;
+using UnityEngine;
+using JetBrains.Annotations;
+
+public class GazeController : MonoBehaviour
+{
+    [SerializeField]
+    private float _distance = 10;
+
+    [SerializeField]
+    private LayerMask _blockingLayers;
+
+    public static event Action<Interactable> OnInteractableHoverEnter;
+    public static event Action<Interactable> OnInteractableHoverExit;
+
+    private Interactable _currentInteractable = null;
+
+    [UsedImplicitly]
+    private void Awake()
+    {
+    }
+
+    [UsedImplicitly]
+    private void Update()
+    {
+        Interactable interactable = GetGazeSelection();
+
+        if (_currentInteractable != interactable)
+        {
+            Debug.LogFormat("Changed interactable from {0} to {1}", _currentInteractable, interactable);
+            if (OnInteractableHoverExit != null) OnInteractableHoverExit(_currentInteractable);
+            _currentInteractable = interactable;
+            if (OnInteractableHoverEnter != null) OnInteractableHoverEnter(_currentInteractable);
+        }
+    }
+
+    private Interactable GetGazeSelection()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, transform.forward, out hit, _distance, _blockingLayers);
+
+#if DEBUG_RAYCAST
+        Debug.DrawLine(transform.position, transform.position + transform.forward * _distance, Color.green, Time.deltaTime);
+#endif
+
+        if (hit.collider == null)
+            return null;
+
+        return hit.collider.gameObject.GetComponent<Interactable>();
+    }
+}
---
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Renderer))]
+public class Interactable : MonoBehaviour
+{
+    public Renderer RendererObject { get; private set; }
+
+    private void Awake()
+    {
+        RendererObject = GetComponent<Renderer>();
+    }
+}
