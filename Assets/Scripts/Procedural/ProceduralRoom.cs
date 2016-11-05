38b7fede77d8db10b9a09c036c4681da16eda4fb
sprawls <alex3521@hotmail.fr>
2016-11-05 18:42:13 -0400

    Added unspawning of procedural rooms by colliders and distance to player

--- Assets/Scripts/Procedural/ProceduralRoom.cs
+++ Assets/Scripts/Procedural/ProceduralRoom.cs
@@ -8,10 +8,16 @@ public class ProceduralRoom : MonoBehaviour {
     [SerializeField] private RoomAnchor endAnchor;
     [SerializeField] private GameObject RoomToSpawn;
 
+    [SerializeField] private float UnspawnDistance = 100f;
+    private static GameObject player;
+
 
     void Start() {
         if (startAnchor == null) Debug.LogWarning("Start Anchor is null for " + gameObject.name);
         if (RoomToSpawn == null) Debug.LogWarning("No room to spawn for " + gameObject.name);
+        if (player == null) player = GameObject.FindGameObjectWithTag("Player");
+
+        StartCoroutine(HandleUnspawnDistance());
     }
 
     public void PositionRoomToOtherAnchor(RoomAnchor otherAnchor) {
@@ -30,6 +36,19 @@ public class ProceduralRoom : MonoBehaviour {
     public void SpawnEndRoom() {
         GameObject go = (GameObject)Instantiate(RoomToSpawn, transform.position, Quaternion.identity);
         go.GetComponent<ProceduralRoom>().PositionRoomToOtherAnchor(endAnchor);
+    }
+
+    public void Remove() {
+        Destroy(gameObject);
+    }
+
+    IEnumerator HandleUnspawnDistance() {
+        while (true) {
+            if (player != null) {
+                if ((transform.position - player.transform.position).magnitude > UnspawnDistance) Remove();
+                yield return new WaitForSeconds(1f);
+            }
+        }
     }
 
 
