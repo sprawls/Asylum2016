e1595414ea2f50508fce2673dd8926dcb1c8850c
sprawls <alex3521@hotmail.fr>
2016-11-04 20:44:41 -0400

    basic noise script

--- Assets/Scripts/CameraDistortionVision.cs
+++ Assets/Scripts/CameraDistortionVision.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 [ExecuteInEditMode]
-[AddComponentMenu("Image Effects/Night Vision")]
+[AddComponentMenu("Image Effects/CameraDistortion")]
 
 public class CameraDistortionVision : MonoBehaviour {
     // public data
@@ -14,7 +14,7 @@ public class CameraDistortionVision : MonoBehaviour {
     //-----------------------------------------
     // start 
     void Start() {
-        shader = Shader.Find("Image Effects/Night Vision");
+        shader = Shader.Find("Image Effects/CameraDistortion");
         mat = new Material(shader);
         mat.SetVector("lum", new Vector4(luminence.g, luminence.g, luminence.g, luminence.g));
         mat.SetFloat("noiseFactor", noiseFactor);
