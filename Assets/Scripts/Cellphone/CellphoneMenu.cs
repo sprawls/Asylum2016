6d494783fdcb254e5d0d61ecf30ecb91feb1fc67
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-11-04 22:57:55 -0400

    Cellphone menu stub

--- Assets/Scripts/Cellphone/CellphoneMenu.cs
+++ Assets/Scripts/Cellphone/CellphoneMenu.cs
@@ -1,6 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class CellphoneMenu : MonoBehaviour {
+	[Header("Apps")]
+	public Button phoneButton;
+	public Button cameraButton;
+	public Button flashlightButton;
+	public Button menuButton;
+
 	void Start () {
 	
 	}
