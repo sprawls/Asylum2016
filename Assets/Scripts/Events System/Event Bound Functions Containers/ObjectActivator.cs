0c3c35cc05d7ea1f73baaefe3f93ed0d8af24445
Antoine Brouillette <antoineqc@hotmail.com>
2016-11-06 16:06:46 -0500

    Added useful functions to event system

---
+++ Assets/Scripts/Events System/Event Bound Functions Containers/ObjectActivator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectActivator : MonoBehaviour, IEventBoundFunctions
+{
+    [EventBoundFunction]
+    public void ActivateObject()
+    {
+        gameObject.SetActive(true);
+    }
+
+    [EventBoundFunction]
+    public void DeactivateObject()
+    {
+        gameObject.SetActive(false);
+    }
+}
