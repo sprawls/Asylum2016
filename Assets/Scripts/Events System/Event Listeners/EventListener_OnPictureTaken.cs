b32f7157ed40cefb918424b7086be75a3b4a2a2c
Antoine Brouillette <antoineqc@hotmail.com>
2016-11-06 16:41:15 -0500

    Changed event listener to trigger according to CameraSight event

--- Assets/Scripts/Events System/Event Listeners/EventListener_OnPictureTaken.cs
+++ Assets/Scripts/Events System/Event Listeners/EventListener_OnPictureTaken.cs
@@ -1,31 +1,13 @@
 ﻿using UnityEngine;
 using JetBrains.Annotations;
 
-[RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(CameraSight))]
 public class EventListener_OnPictureTaken : EventListener
 {
-    private IsRendered _isRendered;
-
     protected override void Awake()
     {
         base.Awake();
 
-        CameraController.OnPictureTaken += OnPictureTaken;
-
-        _isRendered = gameObject.AddComponent<IsRendered>();
-    }
-
-    [UsedImplicitly]
-    private void OnDestroy()
-    {
-        CameraController.OnPictureTaken -= OnPictureTaken;
-    }
-
-    private void OnPictureTaken(Sprite sprite)
-    {
-        if (_isRendered._visible)
-        {
-            Trigger();
-        }
+        GetComponent<CameraSight>().OnImportantPictureTakenNonStatic += () => { Trigger(); };
     }
 }
