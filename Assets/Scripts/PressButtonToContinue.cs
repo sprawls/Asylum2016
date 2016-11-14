d07df543219c6a3e13b0949dd84000ad0344c8ad
Antoine Brouillette <antoineqc@hotmail.com>
2016-11-13 21:27:31 -0500

    Added MenuScene, only missing the menu decals

---
+++ Assets/Scripts/PressButtonToContinue.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressButtonToContinue : MonoBehaviour
+{
+    [SerializeField] private KeyCode _keycode;
+    [SerializeField] private string _levelToLoad;
+
+    [UsedImplicitly]
+    private void Update()
+    {
+        if (Input.GetKeyDown(_keycode))
+        {
+            SceneManager.LoadScene(_levelToLoad);
+        }
+    }
+}
